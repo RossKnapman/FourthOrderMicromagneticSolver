b19d3fa9ebf073845551c892ee7020f455dd7716
Ross Knapman <rjknapman@gmail.com>
2021-11-22 15:35:25 +0100

    Re-write step to work with new vector field class

--- main.cpp
+++ main.cpp
@@ -31,6 +31,16 @@ void normalise(float (&a)[3], float (&out)[3])
     out[2] = a[2] / magnitude;
 }
 
+void normalise(VectorField<float, N, N, 2> &vec)
+{
+    for (int i=0; i<N; i++)
+        for (int j=0; j<N; j++)
+            float magnitude = sqrt(vec.data[i][j][0] * vec.data[i][j][0] + vec.data[i][j][1] * vec.data[i][j][1] + vec.data[i][j][2] * vec.data[i][j][2];
+            vec[i][j][0] = vec[i][j][0] * (1/magnitude);
+            vec[i][j][1] = vec[i][j][1] * (1/magnitude);
+            vec[i][j][2] = vec[i][j][2] * (1/magnitude);
+}
+
 void initialisePosition(VectorField<float, N, N, 2> &pos)
 {
     for (int i=0; i<N; i++)
@@ -181,120 +191,46 @@ void calculateTimeDerivative(VectorField<float, N, N, 3> &m, VectorField<float, 
     }
 }
 
-// void step(float (&m)[N][N][3], float (&B)[N][N][3])
-// {
+void step(VectorField<float, N, N, 3> &m, VectorField<float, N, N, 3> &B)
+{
 
-//     // Dorman-Prince (RK45) integration
-//     float k1[N][N][3];
-//     float k2[N][N][3];
-//     float k3[N][N][3];
-//     float k4[N][N][3];
-//     float k5[N][N][3];
-//     float k6[N][N][3];
+    // Dorman-Prince (RK45) integration
+    VectorField<float, N, N, 3> k1;
+    VectorField<float, N, N, 3> k2;
+    VectorField<float, N, N, 3> k3;
+    VectorField<float, N, N, 3> k4;
+    VectorField<float, N, N, 3> k5;
+    VectorField<float, N, N, 3> k6;
 
-//     // Calculate k1
-//     calculateTimeDerivative(m, B, k1);
-//     scalarMultiply(h, k1, k1);
 
-//     // Calculate k2
-//     float k1fork2[N][N][3];
-//     float k2Field[N][N][3];
-//     scalarMultiply((1./5.), k1, k1fork2);
-//     arrayAdd2(m, k1fork2, k2Field);
-//     calculateTimeDerivative(k2Field, B, k2);
-//     scalarMultiply(h, k2, k2);
+    // Calculate k1
+    calculateTimeDerivative(m, B, k1);
+    k1 = k1 * h;
 
-//     // Calculate k3
-//     float k1fork3[N][N][3];
-//     float k2fork3[N][N][3];
-//     float k3Field[N][N][3];
-//     scalarMultiply((3./40.), k1, k1fork3);
-//     scalarMultiply((9./40.), k2, k2fork3);
-//     arrayAdd3(m, k1fork3, k2fork3, k3Field);
-//     calculateTimeDerivative(k3Field, B, k3);
-//     scalarMultiply(h, k3, k3);
+    // Calculate k2
+    calculateTimeDerivative(m + k1*(1./5.), B, k2);
+    k2 = k2 * h;
 
-//     // Calculate k4
-//     float k1fork4[N][N][3];
-//     float k2fork4[N][N][3];
-//     float k3fork4[N][N][3];
-//     float k4Field[N][N][3];
-//     scalarMultiply((44./45.), k1, k1fork4);
-//     scalarMultiply(-(56./15.), k2, k2fork4);
-//     scalarMultiply((32./9.), k3, k3fork4);
-//     arrayAdd4(m, k1fork4, k2fork4, k3fork4, k4Field);
-//     calculateTimeDerivative(k4Field, B, k4);
-//     scalarMultiply(h, k4, k4);
+    // Calculate k3
+    calculateTimeDerivative(m + k1*(3./40.) + k2*(9./40.), B, k3);
+    k3 = k3 * h;
 
-//     // Calculate k5
-//     float k1fork5[N][N][3];
-//     float k2fork5[N][N][3];
-//     float k3fork5[N][N][3];
-//     float k4fork5[N][N][3];
-//     float k5Field[N][N][3];
-//     scalarMultiply((19372./6561.), k1, k1fork5);
-//     scalarMultiply(-(25360./2187.), k2, k2fork5);
-//     scalarMultiply((64448./6561), k3, k3fork5);
-//     scalarMultiply(-(212./729.), k4, k4fork5);
-//     arrayAdd5(m, k1fork5, k1fork5, k3fork5, k4fork5, k5Field);
-//     calculateTimeDerivative(k5Field, B, k5);
-//     scalarMultiply(h, k5, k5);
+    // Calculate k4
+    calculateTimeDerivative(m + k1*(44./45.) - k2*(56./15.) + k3*(32./9/), B, k4)
+    k4 = k4 * h;
 
-//     // Calculate k6
-//     float k1fork6[N][N][3];
-//     float k2fork6[N][N][3];
-//     float k3fork6[N][N][3];
-//     float k4fork6[N][N][3];
-//     float k5fork6[N][N][3];
-//     float k6Field[N][N][3];
-//     scalarMultiply((9017./3168.), k1, k1fork6);
-//     scalarMultiply(-(355./33.), k2, k2fork6);
-//     scalarMultiply(-(46732./5247.), k3, k3fork6);
-//     scalarMultiply((49./176.), k4, k4fork6);
-//     scalarMultiply(-(5103./18656.), k5, k5fork6);
-//     arrayAdd6(m, k1fork6, k2fork6, k3fork6, k4fork6, k5fork6, k6Field);
-//     calculateTimeDerivative(k6Field, B, k6);
-//     scalarMultiply(h, k6, k6);
+    // Calculate k5
+    calculateTimeDerivative(m + k1*(19372./6561.) - k2*(25360./2187.) + k3*(64448./6561.) - k4*(212./729.), B, k5);
+    k5 = k5 * h;
 
-//     float k1Final[N][N][3];
-//     float k3Final[N][N][3];
-//     float k4Final[N][N][3];
-//     float k5Final[N][N][3];
-//     float k6Final[N][N][3];
-//     scalarMultiply((35./384.), k1, k1Final);
-//     scalarMultiply((500./1113.), k3, k3Final);
-//     scalarMultiply((125./192.), k4, k4Final);
-//     scalarMultiply(-(2187./6784.), k5, k5Final);
-//     scalarMultiply((11./84.), k6, k6Final);
+    // Calculate k6
+    calculateTimeDerivative(m + k1*(9017./3168.) - k2*(355./33.) - k3*(46732./5247.) + k4*(49./176.) - k5*(5103./18656.), B, k6);
+    k6 = k6 * h;
 
-//     for (int i=0; i<N; i++)
-//     {
-//         for (int j=0; j<N; j++)
-//         {
-//             m[i][j][0] += k1Final[i][j][0];
-//             m[i][j][1] += k1Final[i][j][1];
-//             m[i][j][2] += k1Final[i][j][2];
-
-//             m[i][j][0] += k3Final[i][j][0];
-//             m[i][j][1] += k3Final[i][j][1];
-//             m[i][j][2] += k3Final[i][j][2];
-
-//             m[i][j][0] += k4Final[i][j][0];
-//             m[i][j][1] += k4Final[i][j][1];
-//             m[i][j][2] += k4Final[i][j][2];
-
-//             m[i][j][0] += k5Final[i][j][0];
-//             m[i][j][1] += k5Final[i][j][1];
-//             m[i][j][2] += k5Final[i][j][2];
-
-//             m[i][j][0] += k6Final[i][j][0];
-//             m[i][j][1] += k6Final[i][j][1];
-//             m[i][j][2] += k6Final[i][j][2];
-
-//             normalise(m[i][j], m[i][j]);
-//         }
-//     }
-// }
+    m = m + k1*(35./384.) + k3*(500./1113.) + k4*(125./192.) - k5*(2187./6784.) + k6*(11./64.);
+    normalise(m);
+    }
+}
 
 void writeFile(string name, VectorField<float, N, N, 3> &m)
 {
@@ -363,7 +299,13 @@ int main()
     calculateTimeDerivative(m, B, dmdt);
     writeFile("data/m000000.ovf", dmdt);
 
-    std::cout << to_string(dmdt.data[0][0][0]) << " " << to_string(dmdt.data[0][0][1]) << " " << to_string(dmdt.data[0][0][2]) << endl;
+    VectorField<float, 1, 1, 3> test;
+    test.data[0][0][0] = 10;
+    test.data[0][0][1] = 21;
+    test.data[0][0][2] = -100;
+    normalise(test);
+
+    std::cout << to_string(test.data[0][0][0]) << " " << to_string(test.data[0][0][1]) << " " << to_string(test.data[0][0][2]) << endl;
 
 
     // int counter = 0;
