a74ca5f974a052d840e4f4e10beaca8198b0f89f
Ross Knapman <rjknapman@gmail.com>
2021-11-25 13:32:13 +0100

    Vector field normalisation function and tests

--- src/Maths.hpp
+++ src/Maths.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
+#include "Constants.hpp"
+#include "VectorField.hpp"
+
+#include <cmath>
+
 void crossProduct(float* a0, float* a1, float* a2, float* b0, float* b1, float* b2, float* out0, float* out1, float* out2)
 {
     (*out0) = (*a1)*(*b2) - (*b1)*(*a2);
     (*out1) = (*a2)*(*b0) - (*a0)*(*b2);
     (*out2) = (*a0)*(*b1) - (*a1)*(*b0);
+}
+
+
+void normalise(VectorField *vec)
+{
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            float magnitude = sqrt(vec->data[vec->getIndex(i, j, 0)] * vec->data[vec->getIndex(i, j, 0)] + vec->data[vec->getIndex(i, j, 1)] * vec->data[vec->getIndex(i, j, 1)] + vec->data[vec->getIndex(i, j, 2)] * vec->data[vec->getIndex(i, j, 2)]);
+            vec->data[vec->getIndex(i, j, 0)] = vec->data[vec->getIndex(i, j, 0)] * (1/magnitude);
+            vec->data[vec->getIndex(i, j, 1)] = vec->data[vec->getIndex(i, j, 1)] * (1/magnitude);
+            vec->data[vec->getIndex(i, j, 2)] = vec->data[vec->getIndex(i, j, 2)] * (1/magnitude);
+        }
+    }
 }
--- test/Maths_test.cpp
+++ test/Maths_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "Maths.hpp"
 
+#include <cmath>
+
 TEST(MathsTest, CrossProduct)
 {
     float a0 = 1;
@@ -15,7 +17,25 @@ TEST(MathsTest, CrossProduct)
 
     crossProduct(&a0, &a1, &a2, &b0, &b1, &b2, &out0, &out1, &out2);
 
-    EXPECT_EQ(out0, 19.);
-    EXPECT_EQ(out1, -25.);
-    EXPECT_EQ(out2, -27.);
+    EXPECT_FLOAT_EQ(out0, 19.);
+    EXPECT_FLOAT_EQ(out1, -25.);
+    EXPECT_FLOAT_EQ(out2, -27.);
+}
+
+TEST(MathsTest, NormaliseTest)
+{
+    // Test normalisation of the vector field
+    VectorField *vectorField = new VectorField(N, N, 3);
+    int i = 50;
+    int j = 71;
+    int xIndex = vectorField->getIndex(i, j, 0);
+    int yIndex = vectorField->getIndex(i, j, 1);
+    int zIndex = vectorField->getIndex(i, j, 2);
+    vectorField->data[xIndex] = 1;
+    vectorField->data[yIndex] = 2;
+    vectorField->data[zIndex] = -3;
+    normalise(vectorField);
+    EXPECT_FLOAT_EQ(vectorField->data[xIndex], 1/sqrt(14));
+    EXPECT_FLOAT_EQ(vectorField->data[yIndex], 2/sqrt(14));
+    EXPECT_FLOAT_EQ(vectorField->data[zIndex], -3/sqrt(14));
 }
