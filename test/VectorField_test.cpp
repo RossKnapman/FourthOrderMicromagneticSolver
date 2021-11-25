d005b7bbac36bd6f9a7fb9fb0fe2803b4c3b24e6
Ross Knapman <rjknapman@gmail.com>
2021-11-25 13:32:49 +0100

    Unit tests test float values

--- test/VectorField_test.cpp
+++ test/VectorField_test.cpp
@@ -18,7 +18,7 @@ TEST(VectorFieldTest, AssignAndRecall)
     VectorField *vectorField = new VectorField(N, N, 3);
     vectorField->data[vectorField->getIndex(3, 4, 5)] = 5.;
     float outValue = vectorField->data[vectorField->getIndex(3, 4, 5)];
-    EXPECT_EQ(outValue, 5.);
+    EXPECT_FLOAT_EQ(outValue, 5.);
 }
 
 TEST(VectorFieldTest, CalculateIndex)
@@ -42,7 +42,7 @@ TEST(VectorFieldTest, ElementWiseAddition)
     b->data[4] = 4;
     VectorField *result = new VectorField(2, 2, 2);
     *result = a->add(*b);
-    EXPECT_EQ(result->data[4], 7);
+    EXPECT_FLOAT_EQ(result->data[4], 7);
 }
 
 TEST(VectorFieldTest, ElementWiseSubtraction)
@@ -54,7 +54,7 @@ TEST(VectorFieldTest, ElementWiseSubtraction)
     b->data[4] = 4;
     VectorField *result = new VectorField(2, 2, 2);
     *result = a->subtract(*b);
-    EXPECT_EQ(result->data[4], -1);
+    EXPECT_FLOAT_EQ(result->data[4], -1);
 }
 
 TEST(VectorFieldTest, MultipleElementWiseAddition)
@@ -68,7 +68,7 @@ TEST(VectorFieldTest, MultipleElementWiseAddition)
     c->data[4] = 10;
     VectorField *result = new VectorField(2, 2, 2);
     *result = a->add(b->add(*c));
-    EXPECT_EQ(result->data[4], 17);
+    EXPECT_FLOAT_EQ(result->data[4], 17);
 }
 
 TEST(VectorFieldTest, MultipleElementwiseSubtraction)
@@ -82,7 +82,7 @@ TEST(VectorFieldTest, MultipleElementwiseSubtraction)
     c->data[4] = 10;
     VectorField *result = new VectorField(2, 2, 2);
     *result = a->add(b->subtract(*c));
-    EXPECT_EQ(result->data[4], -3);
+    EXPECT_FLOAT_EQ(result->data[4], -3);
 }
 
 TEST(VectorFieldTest, MultiplyByScalar)
@@ -91,5 +91,5 @@ TEST(VectorFieldTest, MultiplyByScalar)
     VectorField *vectorField = new VectorField(256, 256, 3);
     vectorField->data[400] = 5.;
     *vectorField = vectorField->multiplyByScalar(5.);
-    EXPECT_EQ(vectorField->data[400], 25.);
+    EXPECT_FLOAT_EQ(vectorField->data[400], 25.);
 }
